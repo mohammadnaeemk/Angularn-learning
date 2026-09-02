1da0c949d5fb62e65cba21549c6f6594e56b4dde
naeem_karimzadeh <karimzadeh.m@fixa24.com>
2026-09-02 11:19:40 +0330

    icon sax fixed . angular material fixed . header designed.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+
+import localeFa from '@angular/common/locales/fa';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideAppInitializer(() => {
+      registerLocaleData(localeFa, 'fa');
+    }),
+    { provide: LOCALE_ID, useValue: 'fa' },
     provideRouter(routes), provideClientHydration()
   ]
 };
