2f83e913505e702d8550d1f3284e66cbf8eb55fd
naeem_karimzadeh <karimzadeh.m@fixa24.com>
2026-09-06 11:27:59 +0330

    food carts added and design fixed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Header } from './layout/header/header';
+import { FoodCart } from './features/foodCards/food-cart';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, FoodCart],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/features/foodCards/food-cart.ts
+++ src/app/features/foodCards/food-cart.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RecipeModel } from '../../models';
+import { MOCK_RECIPES } from '../../mock-recipes';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   templateUrl: 'food-cart.html',
-  standalone: true,
+  styleUrl: 'food-cart.css',
+  selector: 'app-food-cart',
+  imports: [MatTableModule, MatIconModule],
 })
 export class FoodCart {
-    
+  protected readonly MOC_DATA = signal<RecipeModel[]>(MOCK_RECIPES);
+  protected activeIndex = signal<number>(Math.floor(Math.random() * MOCK_RECIPES.length) as number);
+  protected readonly selectedFood = signal<RecipeModel>(this.MOC_DATA()[this.activeIndex()]);
+  protected readonly displayedColumns: string[] = ['id', 'name', 'quantity', 'unit'];
+
+  changeFood(moveAction: 'Next' | 'Back') {
+    switch (moveAction) {
+      case 'Next':
+        this.activeIndex.update((value) => {
+          if (value != this.MOC_DATA().length - 1) {
+            return value + 1;
+          }
+          return value;
+        });
+        break;
+      case 'Back':
+        this.activeIndex.update((value) => {
+          if (value != 0) {
+            return value - 1;
+          }
+          return value;
+        });
+        break;
+    }
+  }
 }
