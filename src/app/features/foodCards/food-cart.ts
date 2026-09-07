4554a89725b835bdbb4826975dc8b49f08bd3577
naeem_karimzadeh <karimzadeh.m@fixa24.com>
2026-09-07 14:20:40 +0330

    cart design fixed.

--- src/app/features/foodCards/food-cart.ts
+++ src/app/features/foodCards/food-cart.ts
@@ -3,18 +3,23 @@ import { RecipeModel } from '../../models';
 import { MOCK_RECIPES } from '../../mock-recipes';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   templateUrl: 'food-cart.html',
   styleUrl: 'food-cart.css',
   selector: 'app-food-cart',
-  imports: [MatTableModule, MatIconModule],
+  imports: [MatTableModule, MatIconModule, MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class FoodCart {
   protected readonly MOC_DATA = signal<RecipeModel[]>(MOCK_RECIPES);
   protected activeIndex = signal<number>(Math.floor(Math.random() * MOCK_RECIPES.length) as number);
   protected readonly selectedFood = signal<RecipeModel>(this.MOC_DATA()[this.activeIndex()]);
   protected readonly displayedColumns: string[] = ['id', 'name', 'quantity', 'unit'];
+  protected readonly searchTerm = signal('');
 
   changeFood(moveAction: 'Next' | 'Back') {
     switch (moveAction) {
